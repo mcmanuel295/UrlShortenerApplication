f4a4450102f4b85fd51df7f715ea1a094424be57
unknown <mcmanuel755@gmail.com>
2026-08-19 01:24:53 +0100

    edited the controller

--- src/main/java/com/mcmanuel/UrlShortener/controller/UrlShortenerController.java
+++ src/main/java/com/mcmanuel/UrlShortener/controller/UrlShortenerController.java
@@ -1,7 +1,9 @@
 package com.mcmanuel.UrlShortener.controller;
 
+import com.mcmanuel.UrlShortener.pojo.UrlRequest;
 import com.mcmanuel.UrlShortener.service.UrlShortenerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,13 +11,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin("*")
+@Slf4j
 @RequestMapping("/api/v1/url")
 public class UrlShortenerController {
     private final UrlShortenerService shortenerService;
 
-    @PostMapping("/")
-    ResponseEntity<String> shortenUrl(String url){
-        String shortUrl = shortenerService.createShortUrl(url);
+    @GetMapping("/")
+    public String home() {
+        // Points to /src/main/resources/static/index.html
+        // or /src/main/resources/templates/index.html
+        return "forward:/index.html";
+    }
+
+    @PostMapping("/shorten")
+    ResponseEntity<String> shortenUrl(@RequestBody UrlRequest request){
+        log.info("incoming request {}",request.url());
+
+        String shortUrl = shortenerService.createShortUrl(request.url());
+        System.out.println("original url is "+request.url()+" short code is "+shortUrl);
         if (shortUrl == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
@@ -23,7 +36,7 @@ public class UrlShortenerController {
     }
 
     @GetMapping("/short-code")
-    ResponseEntity<String> getShortCode(String originalUrl){
+    ResponseEntity<String> getShortCode(@RequestBody String originalUrl){
         String shortUrl = shortenerService.getShortCode(originalUrl);
         if (shortUrl == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
@@ -33,7 +46,7 @@ public class UrlShortenerController {
 
 
     @GetMapping("/original-url")
-    ResponseEntity<String> redirectUrl(String shortCode){
+    ResponseEntity<String> redirectUrl(@PathVariable String shortCode){
         String shortUrl = shortenerService.getOriginalUrl(shortCode);
         if (shortUrl == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
